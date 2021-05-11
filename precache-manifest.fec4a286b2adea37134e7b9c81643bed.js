4f3e4f681ce6592b10dc51dabdbc6c89de63872e
Chie-7 <cmuraoka21@gmail.com>
2021-05-12 00:35:38 +0900

    Updates

---
+++ precache-manifest.fec4a286b2adea37134e7b9c81643bed.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4b2a92a532242355ee",
+    "url": "/todo-app/css/app.228a235f.css"
+  },
+  {
+    "revision": "1dd66cdc84a2260391e0a06bd2a17246",
+    "url": "/todo-app/img/bg_lb.1dd66cdc.png"
+  },
+  {
+    "revision": "4143404632a87dc6d2b82743bcac0fa1",
+    "url": "/todo-app/img/bg_lt.41434046.png"
+  },
+  {
+    "revision": "75aba9cb23ba9cf134ef74e1b7e27b6d",
+    "url": "/todo-app/img/bg_rb.75aba9cb.png"
+  },
+  {
+    "revision": "9cfd439142bbaf2d997ac1b3e0bce2b8",
+    "url": "/todo-app/img/bg_rt.9cfd4391.png"
+  },
+  {
+    "revision": "87bf97c23a94dd4f3f1d7332d2507407",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "3c4b2a92a532242355ee",
+    "url": "/todo-app/js/app.93308a20.js"
+  },
+  {
+    "revision": "b22b2a7f7f832918bcfa",
+    "url": "/todo-app/js/chunk-vendors.3e73126d.js"
+  },
+  {
+    "revision": "f89eae5c3c4e7439a9401f6cbf9c5e32",
+    "url": "/todo-app/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/todo-app/robots.txt"
+  }
+]);
